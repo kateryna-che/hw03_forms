9717c4483ec62c4c95c6a909eb1a9c2a9fcb106b
Absent-kai <absent.kai@gmail.com>
2022-05-22 21:02:55 +0300

    r1

--- yatube/about/urls.py
+++ yatube/about/urls.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 app_name = 'about'
 
 urlpatterns = [
-    path('author/', TemplateView.as_view(template_name='about/author.html'), name='author'),
-    path('tech/', TemplateView.as_view(template_name='about/tech.html'), name='tech'),
+    path('author/', TemplateView.as_view(template_name='about/author.html'),
+         name='author'),
+    path('tech/', TemplateView.as_view(template_name='about/tech.html'),
+         name='tech'),
 ]
